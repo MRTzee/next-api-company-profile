bcf8bf3faec78fc1520c530a45b527ba93ff6177
MRTzee <teukumuksalmina280800@gmail.com>
2024-03-25 11:00:48 +0700

    fix seo9

--- src/app/teams/page.tsx
+++ src/app/teams/page.tsx
@@ -13,7 +13,9 @@ const Teams = () => {
   useEffect(() => {
     const fetchTeamMembers = async () => {
       try {
-        const response = await fetch("https://randomuser.me/api/?results=9");
+        const response = await fetch("https://randomuser.me/api/?results=9", {
+          next: { revalidate: 20 },
+        });
         const data = await response.json();
         setTeamMembers(data.results);
       } catch (error) {
